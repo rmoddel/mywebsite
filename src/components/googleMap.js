1ebb9e25b2f2761bd0bf007cde863faea7ea08d7
rmoddel <rmoddel@gmail.com>
2020-02-24 22:21:28 -0500

    missing =

--- src/components/googleMap.js
+++ src/components/googleMap.js
@@ -14,7 +14,7 @@ export default function App (){
   return (
       <WrappedMap
         isMarkerShown
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key${process.env.GATSBY_GOOGLE_API_KEY}`}
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.GATSBY_GOOGLE_API_KEY}`}
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: `600px` }} />}
         mapElement={<div style={{ height: `100%` }} />}

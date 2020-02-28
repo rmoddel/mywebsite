81f82b26fa03f3b532ab9488ace88c0578845afd
rmoddel <rmoddel@gmail.com>
2020-02-28 11:46:57 -0500

    remove reference to document

--- src/pages/donate.js
+++ src/pages/donate.js
@@ -3,15 +3,12 @@ import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 
 const Landing = (props) => {
-  const x = document ? document.referrer : null;
-  console.log('referer', x);
-  const redirectURL = x ? "/" : "https://pay.banquest.com/gaonyaakov";
   return (
     <Layout>
       <Helmet>
         <title>MGY ~ Donate</title>
         <meta name="description" content="Donate page"/>
-        <meta http-equiv="refresh" content={`0; url=${redirectURL}`} />
+        <meta http-equiv="refresh" content={`0; url=https://pay.banquest.com/gaonyaakov`} />
       </Helmet>
       <section id="banner" className="style2">
         <div className="inner">

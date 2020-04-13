fa7ba04dd9abc582a5131c3f7498ab0e945d9c62
rmoddel <rmoddel@gmail.com>
2020-04-13 10:49:14 -0400

    add raffle winners

--- src/pages/raffle.js
+++ src/pages/raffle.js
@@ -1,19 +1,27 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import Layout from '../components/layout'
+import useWindowSize from 'react-use/lib/useWindowSize'
+import Confetti from 'react-confetti'
 
 const Landing = (props) => {
+  const { width, height } = useWindowSize();
   return (
     <Layout>
       <Helmet>
         <title>MGY ~ Purim Raffle</title>
-        <meta name="description" content="Donate page"/>
-        <meta http-equiv="refresh" content={`0; url=https://secure.cardknox.com/mesivtagaonyaakov`} />
+        <meta name="description" content="Raffle page"/>
       </Helmet>
       <section id="banner" className="style2">
         <div className="inner">
           <header className="major">
-            <h1>Pesach Raffle</h1>
+            <h1>Pesach Raffle:     And the Winners are.....</h1>
+            <h3>The Shaitel: Moshe Kramer | The Package: Dovy Pinter</h3>
+            The Raffle may be over, but you can still donate: <button><a href="/donate">Donate</a></button>
+              <Confetti
+                width={width}
+                height={height}
+              />
           </header>
         </div>
       </section>

dc8004d09ba8c19c42fdf65f75ae8f35431129bc
rmoddel <rmoddel@gmail.com>
2020-04-21 13:51:54 -0400

    remove Pesach Raffle (large) button

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,7 +5,8 @@ const Banner = (props) => (
     <section id="banner" className="major">
         <div className="inner">
             <header className="major">
-                <h1>Welcome to MGY! <Link to="/raffle" className="button next scrolly">PESACH RAFFLE</Link></h1>
+                <h1>Welcome to MGY!</h1>
+                <Link to="/raffle" className="button next scrolly">Pesach Raffle Winners</Link>
             </header>
             <div className="content">
                 <p>A Mesivta High School envisioned with your son in mind</p>

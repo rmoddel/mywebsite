9c2b87dd1584ca5771a7e0101f4f61a583539fdd
rmoddel <rmoddel@gmail.com>
2020-07-13 15:10:53 -0400

    new hotel image

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,12 +6,12 @@ const Banner = (props) => (
         <div className="inner">
             <header className="major">
                 <h1>Welcome to MGY!</h1>
-                <Link to="/raffle" className="button next scrolly">Pesach Raffle Winners</Link>
             </header>
             <div className="content">
                 <p>A Mesivta High School envisioned with your son in mind</p>
                 <ul className="actions">
                   <Link to="/contact" className="button next scrolly">Contact Us</Link>
+                  <Link to="/raffle" className="button next scrolly">Pesach Raffle Winners</Link>
                 </ul>
             </div>
         </div>

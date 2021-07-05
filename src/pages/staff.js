852630daec7b9d38cf73571118cfd1ffebed77a8
rmoddel <rmoddel@gmail.com>
2021-07-04 23:43:03 -0400

    update staff members

--- src/pages/staff.js
+++ src/pages/staff.js
@@ -26,18 +26,20 @@ const Landing = (props) => (
     <div id="main">
       <section id="two" className="spotlights">
         {[
-          {id: 1, name: 'Rabbi Chaim Cohen', position: 'Rosh Yeshiva', img: rabbicohen},
-          {id: 2, name: 'Rabbi Dovid Aschkenasy', position: 'Mashpai/Senior Advisor', img: ''},
-          {id: 3, name: 'Rabbi Moshe Mermelstein', position: 'Menahel', img: ''},
-          {id: 4, name: 'Rabbi Feivy Schreiber',position: '10th grade Magid Shiur',img: ''},
-          {id: 5, name: 'Rabbi Yaakov Kopel Keller',position: '11th grade Magid Shiur',img: ''},
-          {id: 6, name: 'Rabbi Shalom Rosenfeld', position: '9th grade Magid Shiur ~ 2nd Seder', img: ''},
-          {id: 7, name: 'Rabbi Raphael Fried',position: '10th grade Magid Shiur ~ 2nd Seder',img: ''},
-          {id: 8, name: 'Rabbi Raphael Fried',position: '10th grade Magid Shiur ~ 2nd Seder',img: ''},
-          {id: 9, name: 'Rabbi Avrohom Moshe Neirenberg',position: '10th grade Magid Shiur ~ Halacha Seder',img: ''},
-          {id: 10,name: 'Rabbi Shmuel   Gugenheimer',position: '9th grade Magid Shiur ~ Halacha Seder',img: ''},
-          {id: 11,name: 'Rabbi Dovid Berger',position: 'Engish Principal ~ Limudie Chol',img: ''},
-          {id: 12,name: 'Rabbi Heshy Horovitz',position: 'Administrator',img: ''}].map(rebbe => {
+          {id: 1, name: 'Rabbi Chaim Cohen', position: 'Rosh Hayeshiva', img: rabbicohen},
+          {id: 2, name: 'Rabbi Moshe Mermelstein', position: 'Menahel', img: ''},
+          {id: 3, name: 'Rabbi Dovid Aschkenasy', position: 'Mashpai/Senior Advisor', img: ''},
+          {id: 4, name: 'Rabbi Baruch Parnes',position: '12th grade Magid Shiur',img: ''},
+          {id: 4, name: 'Rabbi Elya Keller',position: '12th grade Magid Shiur ~ 2nd Seder',img: ''},
+          {id: 5, name: 'Rabbi Yaakov K. Keller',position: '10th grade Magid Shiur',img: ''},
+          {id: 6, name: 'Rabbi Meshulam Fivesh Schreiber',position: '10th grade Magid Shiur',img: ''},
+          {id: 7, name: 'Rabbi Yaakov Kopel Keller',position: '11th grade Magid Shiur',img: ''},
+          {id: 10,name: 'Rabbi Moshe Schnaidman',position: '10th grade Magid Shiur',img: ''},
+          {id: 9, name: 'Rabbi Raphael Fried',position: '10th grade Magid Shiur ~ 2nd Seder',img: ''},
+          {id: 8, name: 'Rabbi Shalom Rosenfeld', position: '9th grade Magid Shiur ~ 2nd Seder', img: ''},
+          {id: 12,name: 'Rabbi Shmuel Gugenheimer',position: '9th grade Magid Shiur ~ Halacha Seder',img: ''},
+          {id: 13,name: 'Rabbi Dovid Berger',position: 'Engish Principal ~ Limudie Chol',img: ''},
+          {id: 14,name: 'Rabbi Avrumi Korn',position: 'Administrator',img: ''}].map(rebbe => {
           return (
             <section key={rebbe.id}>
               <Link to="/staff" className="image">
